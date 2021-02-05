3799c0bc3a297b26cb6407ae1aba8b0c9147dd85
dwivedi17 <78604309+dwivedi17@users.noreply.github.com>
2021-02-05 19:51:55 +0530

    Update chat-spam-bot.py

--- chat-spam-bot.py
+++ chat-spam-bot.py
@@ -1,14 +1,17 @@
 import pyautogui
 import time
-inp1=input("Enter the text you want to spam: ")
-inp2=float(input("Enter speed of spam(1-50):"))
-inp3=int(input("Enter the time you need to switch to your chat app:"))
-rate=10/inp2 
-def spam(text, speed):
- time.sleep(inp3)
+inp1 = input("Enter the text you want to spam: ")
+inp2 = float(input("Enter speed of spam (1-50): "))
+inp3 = int(input("Enter the number of texts, you want to send: "))
+inp4 = int(input("Enter the time you need to switch to your chat app (in seconds): "))
+def spam(text, speed, repeat, switch_time):
+ print("Switch to you chat app NOW !")
+ time.sleep(switch_time)
+ rate = 10/speed 
  if speed<50 and speed>=1 :
-  while True:
+  while repeat>0 :
     pyautogui.write(text)
-    pyautogui.press("enter")   
-    time.sleep(speed)
-spam(inp1, rate)
+    pyautogui.press("enter")
+    repeat -= 1
+    time.sleep(rate)
+spam(inp1, inp2, inp3, inp4)
